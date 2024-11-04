d17bf883d66c4f07ec9cda6d5e6336e21c141d04
bielzinvx <00001117137703sp@al.educacao.sp.gov.br>
2024-11-04 11:51:07 -0300

    arquivos GS

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'Qual a origem do futebol?',
+    'Inglaterra, século XIX'
+)
+
+criaCartao(
+    'futebol',
+    'Qual o maior jogador de todos os tempos',
+    'pelé'
+)
+
+criaCartao(
+    'futebol',
+    '"Quantos jogadores tem em um time?',
+    '11 jogadores'
+)
+
+criaCartao(
+    'futebol',
+    'Qual a competição mais importante do futebol?',
+    'Copa do mundo da FIFA'
+)
